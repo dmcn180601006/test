b407940a6d16082f8ef10fb9bdf292e07fa855bb
JYB <JYB@JYB-PC>
2018-06-13 21:48:01 +0800

    提交信息

--- src/nn/test.java
+++ src/nn/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Holle work");
+		System.out.println("1248941545");
+		System.out.println("1567459415");
 	}
 
 }
